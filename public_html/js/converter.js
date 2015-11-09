4aaf8ae12c284b300dc36a4bc35c2101d7e95ad7
dennis.schnitzmeier <dennis.schnitzmeier@D128-03w7.rvw.local>
2015-11-09 09:46:46 +0100

    Converter und Datei auslesen implementiert

--- public_html/js/converter.js
+++ public_html/js/converter.js
@@ -1,4 +1,35 @@
 
+var $ = jQuery;
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    $(document).ready(function () 
+    {
+        
+        var obj = JSON.parse(localStorage.getItem('csv'));
+        console.log(obj);
+        $("#upload_button").click(function() { 
+            $("#file_upload").click();
+        });
 
 
+        function handleFileSelect(evt) {
 
+            var files = evt.target.files[0]; // FileList object
+            var reader = new FileReader();
+            var csvData;
+
+            reader.readAsText(files);
+            // files is a FileList of File objects. List some properties.
+            reader.onload = function(e){
+                 csvData = $.csv.toObjects(reader.result, {
+                             separator: ';'
+                         });
+                 localStorage.setItem('csv', JSON.stringify(csvData));
+            };    
+        }
+
+        document.getElementById('file_upload').addEventListener('change', handleFileSelect, false);  
+    });
+} else {
+  alert('Dateiupload wird von Ihrem Browser nicht unterstützt.');
+}
