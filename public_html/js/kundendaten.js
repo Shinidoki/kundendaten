1cd56e7485511826a9e597b5aec1ded3581fece6
thomas.langehenke <thomas.langehenke@D128-02w7.rvw.local>
2015-11-16 12:18:51 +0100

    rowTotal entfernt

--- public_html/js/kundendaten.js
+++ public_html/js/kundendaten.js
@@ -7,24 +7,23 @@ var colModel1 = [];
 for(var key in header){
     colModel1.push({label: key, name: key, width: 100});
 }
-jQuery("#list27").jqGrid({ 
+jQuery("#kundendaten").jqGrid({ 
     datatype: "local",
     data: csv,
     height: 255,
     autowidth: true,
     shrinkToFit: false,
     colModel: colModel1,
-    rowNum:50, 
-    rowTotal: 200, 
+    rowNum:50,
     rowList : [20,30,50], 
     loadonce:true, 
     rownumbers: true, 
     rownumWidth: 40, 
     
     gridview: true, 
-    pager: '#pager27', 
-    sortname: 'item_id', 
+    pager: '#pager', 
+    sortname: 'Adresse', 
     viewrecords: true, 
     sortorder: "asc", 
-    caption: "Loading data from server at once" }); 
-jQuery("#list27").jqGrid('filterToolbar',{searchOperators : true});
+    caption: "Kundendaten" }); 
+jQuery("#kundendaten").jqGrid('filterToolbar',{searchOperators : true});
